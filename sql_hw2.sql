2958a5e54b836c15da1dcba66c7ab12cc6c3bd03
pbal43 <96485166+pbal43@users.noreply.github.com>
2022-06-06 09:40:33 +0500

    Update sql_hw2.sql

--- sql_hw2.sql
+++ sql_hw2.sql
@@ -1,4 +1,4 @@
---1) Создать таблицу employees
+--1) РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ employees
 --- id. serial,  primary key,
 --- employee_name. Varchar(50), not null
 
@@ -8,7 +8,7 @@ employee_name varchar(50) not null
 );
 
 
---2) Наполнить таблицу employees 70 строками. (сделаем через пайтон)
+--2) РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ employees 70 СЃС‚СЂРѕРєР°РјРё. (СЃРґРµР»Р°РµРј С‡РµСЂРµР· РїР°Р№С‚РѕРЅ)
 
 --import psycopg2
 --import names
@@ -22,7 +22,7 @@ employee_name varchar(50) not null
 --
 --cursor = conn.cursor()
 --
---# заполним именами таблицу employees
+--# Р·Р°РїРѕР»РЅРёРј РёРјРµРЅР°РјРё С‚Р°Р±Р»РёС†Сѓ employees
 --for i in range(0,70):
 --    if conn:
 --        print('connected')
@@ -36,8 +36,8 @@ employee_name varchar(50) not null
 --cursor.close()
 --conn.close()
 
---3)	Создать таблицу salary (но она уже была и заполнена ненужными данными. пропишем команду, как создать таблицу,
---затем удалим колонку и добавим колонку (поменять на NULL нельзя), после чего заполним)
+--3)	РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ salary (РЅРѕ РѕРЅР° СѓР¶Рµ Р±С‹Р»Р° Рё Р·Р°РїРѕР»РЅРµРЅР° РЅРµРЅСѓР¶РЅС‹РјРё РґР°РЅРЅС‹РјРё. РїСЂРѕРїРёС€РµРј РєРѕРјР°РЅРґСѓ, РєР°Рє СЃРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ,
+--Р·Р°С‚РµРј СѓРґР°Р»РёРј РєРѕР»РѕРЅРєСѓ Рё РґРѕР±Р°РІРёРј РєРѕР»РѕРЅРєСѓ (РїРѕРјРµРЅСЏС‚СЊ РЅР° NULL РЅРµР»СЊР·СЏ), РїРѕСЃР»Рµ С‡РµРіРѕ Р·Р°РїРѕР»РЅРёРј)
 -- id. Serial  primary key,
 -- monthly_salary. Int, not null
 
@@ -46,7 +46,7 @@ create table salary(
 	monthly_salary int not null
 );
 
--- 4)	Наполнить таблицу salary 15 строками (сделаем вручную):
+-- 4)	РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ salary 15 СЃС‚СЂРѕРєР°РјРё (СЃРґРµР»Р°РµРј РІСЂСѓС‡РЅСѓСЋ):
 --- 1000
 --- 1100
 --- 1200
@@ -82,7 +82,7 @@ values (1000),
 	   (2400),
 	   (2500);
 
---5)	Создать таблицу employee_salary
+--5)	РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ employee_salary
 --- id. Serial  primary key,
 --- employee_id. Int, not null, unique
 --- salary_id. Int, not null
@@ -93,8 +93,8 @@ create table employee_salary(
 	salary_id int not null
 );
 
---6)	Наполнить таблицу employee_salary 40 строками:
---- в 10 строк из 40 вставить несуществующие employee_id
+--6)	РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ employee_salary 40 СЃС‚СЂРѕРєР°РјРё:
+--- РІ 10 СЃС‚СЂРѕРє РёР· 40 РІСЃС‚Р°РІРёС‚СЊ РЅРµСЃСѓС‰РµСЃС‚РІСѓСЋС‰РёРµ employee_id
 
 insert into employee_salary(employee_id, salary_id)
 values (3, 7),
@@ -138,7 +138,7 @@ values (3, 7),
 	   (25, 7),
 	   (28, 8);
 
--- 7)	Создать таблицу roles
+-- 7)	РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ roles
 --- id. Serial  primary key,
 --- role_name. int, not null, unique
 
@@ -147,12 +147,12 @@ id serial primary key,
 role_name int not null unique
 );
 
---8)	Поменять тип столба role_name с int на varchar(30)
+--8)	РџРѕРјРµРЅСЏС‚СЊ С‚РёРї СЃС‚РѕР»Р±Р° role_name СЃ int РЅР° varchar(30)
 
 alter table roles 
 alter column role_name type varchar(30) using role_name::varchar(30);
 
---9)	Наполнить таблицу roles 20 строками
+--9)	РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ roles 20 СЃС‚СЂРѕРєР°РјРё
 insert into roles(role_name)
 values ('Junior_Python_developer'),
 	   ('Middle_Python_developer'),
@@ -175,10 +175,10 @@ values ('Junior_Python_developer'),
 	   ('Middle_Automation_QA_engineer'),
 	   ('Senior_Automation_QA_engineer');
 
---10)	Создать таблицу roles_employee
+--10)	РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ roles_employee
 --- id. Serial  primary key,
---- employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
---- role_id. Int, not null (внешний ключ для таблицы roles, поле id)
+--- employee_id. Int, not null, unique (РІРЅРµС€РЅРёР№ РєР»СЋС‡ РґР»СЏ С‚Р°Р±Р»РёС†С‹ employees, РїРѕР»Рµ id)
+--- role_id. Int, not null (РІРЅРµС€РЅРёР№ РєР»СЋС‡ РґР»СЏ С‚Р°Р±Р»РёС†С‹ roles, РїРѕР»Рµ id)
 
 create table roles_employee(
 	id serial primary key,
@@ -190,7 +190,7 @@ create table roles_employee(
 		references roles(id)
 );
 
---11)	Наполнить таблицу roles_employee 40 строками
+--11)	РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ roles_employee 40 СЃС‚СЂРѕРєР°РјРё
 
 insert into roles_employee(employee_id, role_id)
 values  (7, 2),
